f3c115a9b16434ca7439a0e6dfb1f022e11ff92a
Zach Gallman <zach.ecab@gmail.com>
2023-04-27 10:32:43 -0400

    Create CurrentWeather component

---
+++ src/app/components/CurrentWeather.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const CurrentWeather = ({ weatherData, unit }) => {
+  const { name, main, weather } = weatherData;
+
+  const temperature =
+    unit === "metric"
+      ? main.temp.toFixed(1)
+      : (main.temp * 1.8 + 32).toFixed(1);
+  const feelsLike =
+    unit === "metric"
+      ? main.feels_like.toFixed(1)
+      : (main.temp * 1.8 + 32).toFixed(1);
+
+  return (
+    <div>
+      <h2>Current Weather in {name}</h2>
+      <p>
+        Temperature: {temperature} {unit === "metric" ? "°C" : "°F"}
+      </p>
+      <p>
+        Feels like: {feelsLike} {unit === "metric" ? "°C" : "°F"}
+      </p>
+      <p>Humidity: {main.humidity}%</p>
+      <p>
+        Wind Speed: {main.speed} {unit === "metric" ? "m/s" : "mph"}
+      </p>
+      <img
+        src={`http://openweathermap.org/img/w/${weather[0].icon}.png`}
+        alt={weather[0].description}
+      />
+      <p>{weather[0].description}</p>
+    </div>
+  );
+};
+
+export default CurrentWeather;
